044c4175c9bdf24c07bf37b2c268a3bb99db80f0
dzzuze <notinsidik@gmail.com>
2026-01-28 23:54:30 +0300

    feat: add getPromise

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,18 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const testPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (number < 0) {
+        reject(new Error('promise that will be rejected'));
+      } else {
+        resolve('promise that will be fulfilled');
+      }
+    }, 1000);
+  });
+
+  return testPromise;
 }
 
 /**
